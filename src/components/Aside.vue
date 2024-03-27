440e8335ffb623ef52db00298dd57fe0ee7bdf47
Bamfjord <bjorn@bitsector.no>
2024-03-27 09:07:00 +0100

    Nav items

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="border-r border-slate-100 bg-white h-full p-8">
-    <h1>SideBar</h1>
+    <h1 class="text-2xl font-thin">Navigation</h1>
+    <nav class="flex flex-col gap-4 mt-6 font-semibold text-sm">
+      <router-link to="/">Home</router-link>
+      <router-link to="/">Import</router-link>
+      <router-link to="/">Other page</router-link>
+      <router-link to="/">Transactions</router-link>
+      <router-link to="/">Home</router-link>
+    </nav>
   </div>
 </template>
