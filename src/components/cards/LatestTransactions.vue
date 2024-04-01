beed35b95a2d88c6a109a86fc6b53e016f3d297c
bamfjord <bjorn.kamfjord@columbusglobal.com>
2024-04-01 19:39:26 +0200

    Latest transactions 5

--- src/components/cards/LatestTransactions.vue
+++ src/components/cards/LatestTransactions.vue
@@ -1,16 +1,20 @@
 <script setup>
 import { onMounted, ref } from "vue";
-
 import { supabase } from "../../lib/supabaseClient";
 
 const transcations = ref([]);
 const error = ref(null);
 async function getTransactions() {
-  let { data, error } = await supabase.from("transactions").select();
+  let { data, error } = await supabase
+    .from("transactions")
+    .select("Dato,Beløp,Tekst,id")
+    .order("Dato", { ascending: false })
+    .limit(5);
+  transcations.value = data;
+
   if (error) {
     throw new Error(error.message);
   }
-  transcations.value = data;
 }
 
 onMounted(() => {
@@ -24,7 +28,7 @@ onMounted(() => {
     <button @click="fetchTransactions">Refresh</button>
     <ul>
       <li v-for="transaction in transcations" :key="transaction.id">
-        {{ transaction.amount }} - {{ transaction.description }}
+        {{ transaction.Beløp }} - {{ transaction.Tekst }}
       </li>
     </ul>
     <p v-if="error">{{ error }}</p>
